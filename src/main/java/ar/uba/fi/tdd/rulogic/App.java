2f3191bf56dbd3f5c95d1b56fafed34f897fce2a
rodrivaldivia <rodrivaldivia10@gmail.com>
2017-11-05 20:31:26 -0300

    TP terminado con cobertura

--- src/main/java/ar/uba/fi/tdd/rulogic/App.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/App.java
@@ -2,6 +2,8 @@ package ar.uba.fi.tdd.rulogic;
 
 import ar.uba.fi.tdd.rulogic.model.KnowledgeBase;
 
+import java.util.Scanner;
+
 /**
  * Console application.
  *
@@ -12,6 +14,15 @@ public class App
 		
 		System.out.println("I shall answer all your questions!");
 		KnowledgeBase knowledgeBase = new KnowledgeBase();
-		knowledgeBase.parseDB("src/main/resources/rules.db");
+		if(!knowledgeBase.parseDB("src/main/resources/rules.db")){
+			return;
+		}
+		Scanner scanner = new Scanner( System.in );
+		String input;
+		do{
+			System.out.print( "Type your query (\"quit\" to quit program): " );
+			input = scanner.nextLine();
+			System.out.println( "Query = " +  knowledgeBase.answer(input));
+		}while (input.compareTo("quit") != 0);
     }
 }
